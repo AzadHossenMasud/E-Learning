5efd44d88186cad0f539a00afe3bb8f4e151980a
Azad Hossen <azadhossen6677@gmail.com>
2022-10-26 10:39:13 +0600

    route setup

---
+++ src/layout/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../pages/shared/Header/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h2>This is Main</h2>
+        </div>
+    );
+};
+
+export default Main;
